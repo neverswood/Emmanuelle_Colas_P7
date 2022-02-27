c320573be537811235f9e0653e30fbcce19a5cb7
neverswood <nevers_wood@hotmail.fr>
2022-02-27 22:51:17 +0100

    Close tag

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ function index() {
   bindAppliancesDropdownEventListeners(app);
   bindIngredientsDropdownEventListeners(app);
   bindUtensilsDropdownEventListeners(app);
+  bindRemoveTagEventListener(app);
 }
 index();
 
