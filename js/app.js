1a3d42653866b3de3cf631adc0f5fac3788791f3
neverswood <nevers_wood@hotmail.fr>
2022-02-27 22:56:35 +0100

    Filter recipes by tag

--- js/app.js
+++ js/app.js
@@ -14,6 +14,18 @@ export default class App {
     } else {
       this.filteredRecipes = filterRecipesByKeyword(this.keyword);
     }
+    this.filteredRecipes = filterRecipesByAppliances(
+      this.filteredRecipes,
+      this.selectedTags
+    );
+    this.filteredRecipes = filterRecipesByUtensils(
+      this.filteredRecipes,
+      this.selectedTags
+    );
+    this.filteredRecipes = filterRecipesByIngredients(
+      this.filteredRecipes,
+      this.selectedTags
+    );
     if (this.filteredRecipes.length > 0) {
       displayRecipes(this.filteredRecipes);
     } else {
@@ -78,5 +90,121 @@ function filterRecipesByKeyword(keyword) {
     }
   }
 
+  return results;
+}
+
+function filterRecipesByAppliances(recipes, tags) {
+  let filterTags = [];
+  for (let tagsIndex = 0; tagsIndex < tags.length; ++tagsIndex) {
+    if (tags[tagsIndex].type === "appliance") {
+      filterTags.push(tags[tagsIndex]);
+    }
+  }
+  let results = [];
+  if (filterTags.length === 0) {
+    results = recipes;
+  } else {
+    for (let recipesIndex = 0; recipesIndex < recipes.length; recipesIndex++) {
+      let matchAllTags = true;
+      for (let tagsIndex = 0; tagsIndex < filterTags.length; ++tagsIndex) {
+        let applianceMatchTag = recipes[recipesIndex].appliance
+          .toLowerCase()
+          .indexOf(filterTags[tagsIndex].name.toLowerCase());
+        if (applianceMatchTag > -1) {
+          break;
+        }
+        if (applianceMatchTag === -1) {
+          matchAllTags = false;
+          break;
+        }
+      }
+      if (matchAllTags) {
+        results.push(recipes[recipesIndex]);
+      }
+    }
+  }
+  return results;
+}
+
+function filterRecipesByUtensils(recipes, tags) {
+  let filterTags = [];
+  for (let tagsIndex = 0; tagsIndex < tags.length; ++tagsIndex) {
+    if (tags[tagsIndex].type === "utensils") {
+      filterTags.push(tags[tagsIndex]);
+    }
+  }
+  let results = [];
+  if (filterTags.length === 0) {
+    results = recipes;
+  } else {
+    for (let recipesIndex = 0; recipesIndex < recipes.length; recipesIndex++) {
+      let matchAllTags = true;
+      for (let tagsIndex = 0; tagsIndex < filterTags.length; ++tagsIndex) {
+        let utensilMatchTag = -1;
+
+        for (
+          let ustensilsIndex = 0;
+          ustensilsIndex < recipes[recipesIndex].ustensils.length;
+          ++ustensilsIndex
+        ) {
+          utensilMatchTag = recipes[recipesIndex].ustensils[ustensilsIndex]
+            .toLowerCase()
+            .indexOf(filterTags[tagsIndex].name.toLowerCase());
+          if (utensilMatchTag > -1) {
+            break;
+          }
+        }
+        if (utensilMatchTag === -1) {
+          matchAllTags = false;
+          break;
+        }
+      }
+      if (matchAllTags) {
+        results.push(recipes[recipesIndex]);
+      }
+    }
+  }
+  return results;
+}
+
+function filterRecipesByIngredients(recipes, tags) {
+  let filterTags = [];
+  for (let tagsIndex = 0; tagsIndex < tags.length; ++tagsIndex) {
+    if (tags[tagsIndex].type === "ingredients") {
+      filterTags.push(tags[tagsIndex]);
+    }
+  }
+  let results = [];
+  if (filterTags.length === 0) {
+    results = recipes;
+  } else {
+    for (let recipesIndex = 0; recipesIndex < recipes.length; recipesIndex++) {
+      let matchAllTags = true;
+
+      for (let tagsIndex = 0; tagsIndex < filterTags.length; ++tagsIndex) {
+        const ingredients = recipes[recipesIndex].ingredients;
+        let ingredientMatchTag = -1;
+        for (
+          let ingredientsIndex = 0;
+          ingredientsIndex < ingredients.length;
+          ingredientsIndex++
+        ) {
+          ingredientMatchTag = ingredients[ingredientsIndex].ingredient
+            .toLowerCase()
+            .indexOf(filterTags[tagsIndex].name.toLowerCase());
+          if (ingredientMatchTag > -1) {
+            break;
+          }
+        }
+        if (ingredientMatchTag === -1) {
+          matchAllTags = false;
+          break;
+        }
+      }
+      if (matchAllTags) {
+        results.push(recipes[recipesIndex]);
+      }
+    }
+  }
   return results;
 }
